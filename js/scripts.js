1c47d61477b0444724279f09e157c0808acdf70c
Jagruti <j_gohil96@hotmail.co.uk>
2018-12-02 18:19:50 +0000

    Adding functions to buttons
    Buttons on clicking now alert the inavailability of page!

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
-$("input").click(function(){
-  alert("Thank you for subscribing!")
-})
+function Subscribe() {
+  alert ("Thank you for subscribing!")
+  }
+
+function socialmedia() {
+    alert ("Thank you for subscribing! Sadly, we are not yet online :( but will notify you once we are ready!")
+    }
